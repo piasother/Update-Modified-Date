2914bf6ce98e68d1b6bce93d489b09f8bc953de2
piasother <piasother@gmail.com>
2016-07-29 14:19:15 +0100

    commit

--- Update Modified Date/Script.ps1
+++ Update Modified Date/Script.ps1
@@ -1,3 +1,23 @@
 ﻿#
 # Script.ps1
 #
+[System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint")
+if((Get-PSSnapin | Where {$_.Name -eq "Microsoft.SharePoint.PowerShell"}) -eq $null) {
+     Add-PSSnapin Microsoft.SharePoint.PowerShell;
+ }
+
+$web = get-spweb "http://spfarm-sp" 
+$list = $web.Lists["Test List"]
+
+$spSourceItems = $list.Items
+
+#$dateToStore = Get-Date "4/6/2013 10:00:00"
+
+foreach($item in $spSourceItems)
+{
+    Write-Host ("...... ORIG ... last modified by {0} on {1}" -f $item["Editor"].tostring(), $item["Modified"]) -foregroundcolor "blue"  
+    $item["Modified"] = "2014-04-02 10:00:00"
+    $item.Update() 
+    $item.UpdateOverwriteVersion() 
+}
+
